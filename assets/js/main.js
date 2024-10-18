e927794f4b845d9f578100a313f727d9d7cd6b2d
sunny <sunnya2023@gmail.com>
2024-10-19 06:12:49 +0900

    completed new arrival section

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,28 @@ var swiperCategories = new Swiper(".categories__container ", {
   },
 });
 /*=============== SWIPER PRODUCTS ===============*/
-
+var swiperProducts = new Swiper(".new__container ", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+  },
+});
 /*=============== PRODUCTS TABS ===============*/
 const tabs = document.querySelectorAll("[data-target");
 tabContents = document.querySelectorAll("[content]");
